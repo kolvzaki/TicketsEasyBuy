ac56ed67bc7f482b91b5486a423bbd330d8e534d
Natsulight <1090732655@qq.com>
2022-08-07 14:22:28 +0800

    chore: init deploy config

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "antd/dist/antd.min.css"
+import "antd/dist/antd.min.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
